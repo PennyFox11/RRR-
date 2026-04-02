1c27ebff42ce5f234e7458e37fec83e389223719
robotarms <2804461@students.wits.ac.za>
2026-04-02 17:05:31 +0200

    I added an enemy health bar and a script

---
+++ My project/Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
