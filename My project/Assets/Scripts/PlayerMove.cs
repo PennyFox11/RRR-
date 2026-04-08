9f27c852dbd79ee833defd51dcc4235ccc1d5da4
robotarms <2804461@students.wits.ac.za>
2026-04-08 15:57:48 +0200

    Revert "I think I fixed player movement"
    This reverts commit 05f470f269c405c2a59916a01fc5216f5db19c5d.

--- My project/Assets/Scripts/PlayerMove.cs
+++ My project/Assets/Scripts/PlayerMove.cs
@@ -2,27 +2,22 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float MoveSpeed = 5f;       // assign in Inspector
-    private Rigidbody2D rb;            // renamed variable
+    public float MoveSpeed;
+    public Rigidbody2D Rigidbody;
 
-    private Vector2 moveInput;
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        Rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Get player input
-        moveInput.x = Input.GetAxisRaw("Horizontal"); // A/D or Left/Right
-        moveInput.y = Input.GetAxisRaw("Vertical");   // W/S or Up/Down
-        moveInput.Normalize();
-    }
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        direction.Normalize();
 
-    void FixedUpdate()
-    {
-        // Apply movement to Rigidbody
-        rb.linearVelocity = moveInput * MoveSpeed;
+        Rigidbody.linearVelocity = direction * MoveSpeed;
+
     }
 }
