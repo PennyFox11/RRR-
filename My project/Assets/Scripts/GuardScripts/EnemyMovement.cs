4b8563df707260f6361f55ddce20fbe8549d2d29
robotarms <2804461@students.wits.ac.za>
2026-04-12 18:36:00 +0200

    Guard returns to start

--- My project/Assets/Scripts/GuardScripts/EnemyMovement.cs
+++ My project/Assets/Scripts/GuardScripts/EnemyMovement.cs
@@ -1,17 +1,32 @@
+//Title: Creating Simple Enemy AI to Chase the Player (Unity Tutorial | 2D Top Down Shooter)
+//Author: Ketra Games
+// Date: 12 December 2022
+// Availability: https://www.youtube.com/watch?v=XHrWtLZtzy8
+// How I programmed enemy return function: https://discussions.unity.com/t/solved-make-enemy-move-back-to-start-position/633065 
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+
+    private Vector3 startPosition;
+    
     [SerializeField]
     private float _speed;
 
     [SerializeField]
     private float _rotationSpeed;
 
+    [SerializeField]
+    private float _returnSpeed;
+
     private Rigidbody2D _rigidbody;
     private PlayerAwarenessController _playerAwarenessController;
     private Vector2 _targetDirection;
 
+    void Start()
+    {
+        startPosition = transform.position;
+    }
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -33,7 +48,16 @@ public class EnemyMovement : MonoBehaviour
         }
         else
         {
-            _targetDirection = Vector2.zero;
+            Vector2 directionToStart = (startPosition - transform.position);
+
+            if (directionToStart.magnitude > 0.1f) // prevent jitter
+            {
+               _targetDirection = directionToStart.normalized; // https://docs.unity3d.com/ScriptReference/Vector3-normalized.html
+            }
+            else
+            {
+                _targetDirection = Vector2.zero;
+            }
         }
     }
 
@@ -58,6 +82,7 @@ public class EnemyMovement : MonoBehaviour
         }
         else
         {
+            float currentSpeed = _playerAwarenessController.AwareOfPlayer ? _speed : _returnSpeed;
             _rigidbody.linearVelocity = transform.up * _speed;
         }
     }
