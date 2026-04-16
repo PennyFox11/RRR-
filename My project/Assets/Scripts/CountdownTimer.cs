a8ec07adbc01e3f618d5d334b41aea5a21565fa5
robotarms <2804461@students.wits.ac.za>
2026-04-16 13:43:15 +0200

    Fixed Timer, added new scene (currently empty)

--- My project/Assets/Scripts/CountdownTimer.cs
+++ My project/Assets/Scripts/CountdownTimer.cs
@@ -1,5 +1,6 @@
 //https://discussions.unity.com/t/timer-on-trigger-events-2d/789369
 //https://discussions.unity.com/t/can-someone-help-me-make-a-countdown-timer-for-c-i-just-need-help-making-a-countdown-timer/256846/2 
+//https://docs.unity3d.com/6000.3/Documentation/Manual/collider-interactions-ontrigger.html 
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,8 +9,15 @@ using TMPro;
 public class CountdownTimer : MonoBehaviour
 {
     public float countdownTime = 5f;
+    public float originalTime;
     public TextMeshProUGUI timerText;
     private bool isTimerRunning = false;
+
+    void Start()
+    {
+        originalTime = countdownTime;
+        timerText.gameObject.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,11 +35,23 @@ public class CountdownTimer : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && !isTimerRunning)
+        if (other.CompareTag("Player"))
         {
+            countdownTime = originalTime;
             isTimerRunning = true;
+            timerText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isTimerRunning = false;
+            countdownTime = originalTime;
+            timerText.gameObject.SetActive(false);
         }
     }
 
@@ -43,5 +63,6 @@ public class CountdownTimer : MonoBehaviour
     void OnTimerEnd()
     {
         Debug.Log("Time is up!");
+        timerText.gameObject.SetActive(false);
     }
 }
