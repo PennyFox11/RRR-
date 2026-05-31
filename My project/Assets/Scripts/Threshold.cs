1782dd66481b840a6f62e3b2463288b5f25bb494
robotarms <2804461@students.wits.ac.za>
2026-05-31 20:02:11 +0200

    references for scripts

--- My project/Assets/Scripts/Threshold.cs
+++ My project/Assets/Scripts/Threshold.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-public class Threshold : MonoBehaviour
+public class Threshold : MonoBehaviour //this craetes a reference for teh object that vanishes when the key is collected
 {
     private GameObject threshold;
     private bool thresholdDestroyed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Start() 
     {
         threshold = GameObject.FindGameObjectWithTag("Threshold");
     }
