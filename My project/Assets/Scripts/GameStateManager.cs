f45227b8060c94cd7f4d14665b59dedf6f99e41d
robotarms <2804461@students.wits.ac.za>
2026-06-01 12:30:36 +0200

    exit function

--- My project/Assets/Scripts/GameStateManager.cs
+++ My project/Assets/Scripts/GameStateManager.cs
@@ -1,3 +1,15 @@
+//Title: Implement data persistence between scenes (this script is partially based off teh Unity Learn one, with modifications)
+//Author: Unity Learn
+//Date: 2026
+//Code version: Unity 6.0
+//Availability: https://learn.unity.com/tutorial/implement-data-persistence-between-scenes
+
+//Title: Application.Quit
+//Author: Unity Documentation
+//Date: 30 May 2026
+//Code version: Unity 6000.4
+//Availability: https://docs.unity3d.com/6000.4/Documentation/ScriptReference/Application.Quit.html
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -34,6 +46,14 @@ public class GameStateManager : MonoBehaviour
     public void LoadGameScene()
     {
         SceneManager.LoadScene("boss level");
+    }
+
+    public void QuitGame() //allows you to exit game completely in build
+    {
+        Debug.Log("Quitting Game");
+
+        Time.timeScale = 1f; //safety reset
+        Application.Quit();
     }
 
 }
